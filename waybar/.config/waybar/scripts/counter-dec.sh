139f6520dea0c0421cd919f242df3db912e12feb
Julian Baker <jtoad23@gmail.com>
2026-07-08 09:51:05 +0000

    Added a little counter tool to the top waybar QuickTool section

---
+++ waybar/.config/waybar/scripts/counter-dec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COUNTER="$HOME/.local/share/waybar/counter"
+
+value=$(cat "$COUNTER")
+echo $((value - 1)) > "$COUNTER"
+
+pkill -SIGRTMIN+8 waybar
---
+++ waybar/.config/waybar/scripts/counter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+COUNTER="$HOME/.local/share/waybar/counter"
+
+cat "$COUNTER"
