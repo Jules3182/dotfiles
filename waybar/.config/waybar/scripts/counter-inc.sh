1542880070ea8f66c5e56f412e5dea6dfca9f0cf
Julian Baker <jtoad23@gmail.com>
2026-07-14 11:54:31 +0000

    Added a cool little color gradient effect to the counter notifications

--- waybar/.config/waybar/scripts/counter-inc.sh
+++ waybar/.config/waybar/scripts/counter-inc.sh
@@ -3,6 +3,15 @@
 COUNTER="$HOME/.local/share/waybar/counter"
 
 value=$(cat "$COUNTER")
-echo $((value + 1)) > "$COUNTER"
+new=$((value + 1))
+echo "$new" > "$COUNTER"
+
+# Interpolates notification color from green to red between 0 and 30
+r=$((64 + ((28 - 64) * new / 30)))
+g=$((1 + ((216 - 1) * new / 30)))
+b=$((1 + ((40 - 1) * new / 30)))
+
+color="#$(printf '%02X' $r)$((g < 256 ? g : 0))$((b < 256 ? b : 0))"
+hyprctl notify 1 2000 "rgb($((r<256?r:0)),$((g<256?g:0)),$((b<256?b:0)))" "$new"
 
 pkill -SIGRTMIN+8 waybar
